f5ea0dee8a70599194b504693ae0c02eb0d9cd60
erampone <elena.rampone@iit.it>
2020-02-03 17:30:33 +0100

    add sac and td3 residuals to list of available models

--- robot_agents/__init__.py
+++ robot_agents/__init__.py
@@ -7,8 +7,10 @@ from robot_agents.stable_baselines_lib.sac.sac_residual import SAC_residual
 ALGOS = {
     'stable_baselines_lib': {'ddpg': robot_agents.stable_baselines_lib.train_DDPG,
                              'td3': robot_agents.stable_baselines_lib.train_TD3,
+                             'td3_residual': robot_agents.stable_baselines_lib.train_TD3_residual,
                              'deepq': robot_agents.stable_baselines_lib.train_DQN,
-                             'residual_sac': robot_agents.stable_baselines_lib.train_SAC,
+                             'sac': robot_agents.stable_baselines_lib.train_SAC,
+                             'sac_residual': robot_agents.stable_baselines_lib.train_SAC_residual,
                              'her': robot_agents.stable_baselines_lib.train_HER,
                              'her_test': robot_agents.stable_baselines_lib.test_HER,
                              'ddpg_test': robot_agents.stable_baselines_lib.test_DDPG,
