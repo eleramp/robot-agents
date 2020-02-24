67b7b7607701cdc2904eca4c1c2e4a00e9c9a332
Elena Rampone <elena.rampone@iit.it>
2020-02-24 09:55:01 +0000

    add 'SQ_DRL_sisq/icub_reach_forced_grasp_3objs' experiment

--- evaluation/experiment_registration.py
+++ evaluation/experiment_registration.py
@@ -664,6 +664,40 @@ register_experiment({
 })
 
 register_experiment({
+    'name': 'SQ_DRL_sisq/icub_reach_forced_grasp_3objs',
+    'description': 'reaching and forced grasp lift. 3 objs (mustard), static. robot observation augmented with linear and angular velocities. reward is: -10 fall, -1 contact, 10 grasp pose reaching, +100 lifting with success, 0 otherwise.',
+    'tasks': [{'sub_name': '', 'env_id': _env_prefix+'iCubReachResidual-v0', 'seed': 1,
+               'env_params': {
+                            'log_file': '',
+                            'control_arm': 'r',
+                            'control_orientation': 1,
+                            'control_eu_or_quat': 0,
+                            'obj_pose_rnd_std': 0.0, # change only position
+                            'noise_pcl': 0.00,
+                            'use_superq': 1,
+                            'max_steps': 800,
+                            'renders': False}
+              },
+             ],
+    'algo': {'name': 'sac',
+             'RLlibrary': 'stable_baselines_lib',
+             'description': 'HER from stable_baselines library',
+             'params': {'n_timesteps': 500000,
+                        'policy': 'MlpPolicy',  # options: MlpPolicy e cnns ones
+                        'gamma': 0.99,
+                        'learning_rate': 0.0007224206139165605,
+                        'batch_size': 256,
+                        'buffer_size': 10000,
+                        'learning_starts': 1000,
+                        'train_freq': 10,
+                        'ent_coef': 0.1,
+                        'n_cpu_tf_sess':12,
+                        'policy_kwargs': {'layers': [256,256]},
+                        },
+             },
+})
+
+register_experiment({
     'name': 'icub_grasp',
     'description': 'grasping and lift of 1 object. not goal',
     'tasks': [{'sub_name': '', 'env_id': _env_prefix+'iCubGrasp-v0', 'seed': 1,
